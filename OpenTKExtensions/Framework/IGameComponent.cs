eaf62e5b87409529c599a858ad1716f98777a17f
Geoff Thornburrow <geofftnz@gmail.com>
2016-10-15 21:34:44 +1300

    Moved code from Snowscape

---
+++ OpenTKExtensions/Framework/IGameComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenTKExtensions.Framework
+{
+    public interface IGameComponent
+    {
+        // init the component, load any assets, init any graphics resources
+        void Load();
+
+        // release any resources created in Load()
+        void Unload();
+
+        /// <summary>
+        /// Specifies the load order for the components.
+        /// Components are unloaded in reverse order
+        /// </summary>
+        int LoadOrder { get; set; }
+
+        // current state of component
+        ComponentStatus Status { get; }
+    }
+}
