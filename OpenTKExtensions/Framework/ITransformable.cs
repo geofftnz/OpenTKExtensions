cf4725bcececdf95a1046a0459fa7afa879a17e3
Geoff Thornburrow <geofftnz@gmail.com>
2016-10-27 22:03:03 +1300

    breaking change to ITransformable

--- OpenTKExtensions/Framework/ITransformable.cs
+++ OpenTKExtensions/Framework/ITransformable.cs
@@ -8,7 +8,19 @@ namespace OpenTKExtensions.Framework
 {
     public interface ITransformable
     {
-        Matrix4 ModelView { get; set; }
-        Matrix4 Projection { get; set; }
+        /// <summary>
+        /// Matrix describing the current view in world-space.
+        /// </summary>
+        Matrix4 ViewMatrix { get; set; }
+
+        /// <summary>
+        /// Matrix describing the position of this component in the world.
+        /// </summary>
+        Matrix4 ModelMatrix { get; set; }
+
+        /// <summary>
+        /// Matrix describing the transform from world-space to screen-space.
+        /// </summary>
+        Matrix4 ProjectionMatrix { get; set; }
     }
 }
