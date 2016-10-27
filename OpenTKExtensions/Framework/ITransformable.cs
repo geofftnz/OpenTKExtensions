882843fd24373f8039848dd33c7a49fcf72519fc
Geoff Thornburrow <geofftnz@gmail.com>
2016-10-27 21:28:40 +1300

    added ITransformable

---
+++ OpenTKExtensions/Framework/ITransformable.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenTKExtensions.Framework
+{
+    public interface ITransformable
+    {
+        Matrix4 ModelView { get; set; }
+        Matrix4 Projection { get; set; }
+    }
+}
