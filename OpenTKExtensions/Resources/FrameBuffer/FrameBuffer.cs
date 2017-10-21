906f57b6acc6b1f83e965775b11d69d7b2118f08
Geoff Thornburrow <geofftnz@gmail.com>
2017-10-21 22:16:15 +1300

    framebuffer

---
+++ OpenTKExtensions/Resources/FrameBuffer/FrameBuffer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using NLog;
+using OpenTK;
+
+namespace OpenTKExtensions.Resources
+{
+    public class FrameBuffer : ResourceBase, IResource
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public int ID { get; private set; } = -1;
+        public FramebufferTarget Target { get; private set; }
+        public FramebufferErrorCode Status { get; private set; }
+
+        public bool IsLoaded { get { return ID != -1; } }
+
+        public bool IsStatusOK
+        {
+            get
+            {
+                return Status == FramebufferErrorCode.FramebufferComplete;
+            }
+        }
+
+        public FrameBuffer(string name, FramebufferTarget target) : base(name)
+        {
+            Target = target;
+            Status = FramebufferErrorCode.FramebufferUndefined;
+        }
+        public FrameBuffer(string name)
+            : this(name, FramebufferTarget.Framebuffer)
+        {
+        }
+
+        public void Load()
+        {
+            if (!IsLoaded)
+            {
+                ID = GL.GenFramebuffer();
+                log.Info($"FrameBuffer.Load ({Name}) returned {ID}");
+            }
+        }
+
+        public void Unload()
+        {
+            if (IsLoaded)
+            {
+                GL.DeleteFramebuffer(ID);
+                ID = -1;
+            }
+        }
+
+        public void Bind()
+        {
+            Bind(Target);
+        }
+        public void Bind(FramebufferTarget target)
+        {
+            if (!IsLoaded)
+                throw new InvalidOperationException($"FrameBuffer.Bind ({Name}) not loaded");
+
+            GL.BindFramebuffer(target, ID);
+        }
+
+        public FramebufferErrorCode GetStatus()
+        {
+            Status = GL.CheckFramebufferStatus(FramebufferTarget.Framebuffer);
+            return Status;
+        }
+
+        public void ClearColourBuffer(int drawBuffer, Vector4 colour)
+        {
+            GL.ClearBuffer(ClearBuffer.Color, drawBuffer, new float[] { colour.X, colour.Y, colour.Z, colour.W });
+        }
+
+
+    }
+}
