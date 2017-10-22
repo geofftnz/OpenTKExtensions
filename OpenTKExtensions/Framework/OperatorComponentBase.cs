a253e5ac401c7344d71779ff54d7fbb0ae0d8d43
Geoff Thornburrow <geofftnz@gmail.com>
2017-10-22 22:39:54 +1300

    operatorcomponentbase

---
+++ OpenTKExtensions/Framework/OperatorComponentBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKExtensions.Framework
+{
+
+    /// <summary>
+    /// Base class for an "operator" component: one that renders to one or more render targets. 
+    /// </summary>
+    public class OperatorComponentBase : GameComponentBase, IGameComponent, IRenderable, IResizeable
+    {
+        public int DrawOrder { get; set; }
+        public bool Visible { get; set; } = true;
+
+        public void Render(IFrameRenderData frameData)
+        {
+        }
+
+        public void Resize(int width, int height)
+        {
+        }
+    }
+}
