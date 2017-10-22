6a45999bea1c2e76a8bbc92db465b4bc54a086c6
Geoff Thornburrow <geofftnz@gmail.com>
2017-10-22 21:14:32 +1300

    added sampler

---
+++ OpenTKExtensions/Resources/Textures/Sampler.cs
@@ -0,0 +1,96 @@
+﻿using NLog;
+using System;
+using OpenTK.Graphics.OpenGL4;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKExtensions.Resources
+{
+    public class Sampler : ResourceBase, IResource
+    {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
+        public uint ID { get; private set; } = 0;
+        public bool IsLoaded { get { return ID != 0; } }
+
+        public Dictionary<SamplerParameterName, ISamplerParameter> Parameters { get; } = new Dictionary<SamplerParameterName, ISamplerParameter>();
+
+        public Sampler(string name) : base(name)
+        {
+        }
+
+        public Sampler() : base("unnamed-sampler")
+        {
+        }
+
+        public void Load()
+        {
+            if (!IsLoaded)
+            {
+                ID = (uint)GL.GenSampler();
+                log.Info($"Sampler.Load ({Name}) ID {ID}");
+            }
+        }
+
+        public void Unload()
+        {
+            if (IsLoaded)
+            {
+                GL.DeleteSampler(ID);
+            }
+        }
+
+        public void ApplyParameters()
+        {
+            if (IsLoaded)
+            {
+                foreach (var param in Parameters.Values)
+                {
+                    param.Apply(ID);
+                }
+            }
+        }
+
+        public Sampler SetParameter(ISamplerParameter param)
+        {
+            if (Parameters.ContainsKey(param.ParameterName))
+                Parameters[param.ParameterName] = param;
+            else
+                Parameters.Add(param.ParameterName, param);
+
+            return this;
+        }
+
+        public Sampler Set<T>(SamplerParameterName name, T value) where T : struct, IConvertible
+        {
+            SetParameter(SamplerParameter.Create<T>(name, value));
+            return this;
+        }
+
+        public Sampler Bind(TextureUnit textureUnit)
+        {
+            Bind(textureUnit - TextureUnit.Texture0);
+            return this;
+        }
+
+        public Sampler Bind(int textureUnit)
+        {
+            if (IsLoaded)
+            {
+                GL.BindSampler(textureUnit, (int)ID);
+            }
+            return this;
+        }
+        public static void Unbind(int textureUnit)
+        {
+            GL.BindSampler(textureUnit, 0);
+        }
+        public static void Unbind(TextureUnit textureUnit)
+        {
+            Unbind(textureUnit - TextureUnit.Texture0);
+        }
+
+    }
+}
---
+++ OpenTKExtensions/Resources/Textures/SamplerParameter.cs
@@ -0,0 +1,50 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKExtensions.Resources
+{
+    public class SamplerParameter<T> : ISamplerParameter where T : struct, IConvertible
+    {
+        public SamplerParameterName ParameterName { get; set; }
+        public T Value { get; set; }
+
+        public SamplerParameter(SamplerParameterName name, T value)
+        {
+            ParameterName = name;
+            Value = value;
+        }
+
+        public void Apply(uint samplerID)
+        {
+            // HACK this is nasty.
+            if (Value is Enum)
+            {
+                GL.SamplerParameter(samplerID, ParameterName, Convert.ToInt32(Value));
+                return;
+            }
+            if (Value is int)
+            {
+                GL.SamplerParameter(samplerID, ParameterName, Convert.ToInt32(Value));
+                return;
+            }
+            if (Value is float)
+            {
+                GL.SamplerParameter(samplerID, ParameterName, Convert.ToSingle(Value));
+                return;
+            }
+        }
+
+    }
+
+    public static class SamplerParameter
+    {
+        public static SamplerParameter<U> Create<U>(SamplerParameterName name, U value) where U : struct, IConvertible
+        {
+            return new SamplerParameter<U>(name, value);
+        }
+    }
+}
