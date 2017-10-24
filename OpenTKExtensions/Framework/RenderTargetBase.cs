466d317e98ef3a1b47aa056a96153046bedee69c
Geoff Thornburrow <geofftnz@gmail.com>
2017-10-24 22:57:33 +1300

    Added RenderTargetBase to render a collection of components to a GBuffer

---
+++ OpenTKExtensions/Framework/RenderTargetBase.cs
@@ -0,0 +1,51 @@
+﻿using NLog;
+using OpenTK;
+using OpenTKExtensions.Resources;
+using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKExtensions.Framework
+{
+
+    /// <summary>
+    /// Base class for an "operator" component: one that renders to one or more render targets. 
+    /// </summary>
+    public class RenderTargetBase : CompositeGameComponent, IGameComponent, IRenderable, IResizeable, IReloadable
+    {
+        protected GBuffer OutputBuffer;
+
+        public RenderTargetBase(bool wantDepth = false, int width = 256, int height = 256)
+        {
+            Resources.Add(OutputBuffer = new GBuffer("gbuffer", wantDepth, width, height));
+        }
+
+        public void SetOutput(int index, TextureSlotParam texparam)
+        {
+            OutputBuffer.SetSlot(index, texparam);
+        }
+
+        public void SetOutput(int index, Texture texture)
+        {
+            OutputBuffer.SetSlot(index, texture);
+        }
+
+        public override void Render(IFrameRenderData frameData)
+        {
+            OutputBuffer.BindForWriting();
+
+            Components.Render(frameData);
+
+            OutputBuffer.UnbindFromWriting();
+        }
+
+        public override void Resize(int width, int height)
+        {
+            base.Resize(width, height);
+            OutputBuffer?.Resize(width, height);
+        }
+    }
+}
