af816964cdaf8b5347d3a756eeed096518792dce
Geoff Thornburrow <geofftnz@gmail.com>
2020-03-08 16:47:51 +1300

    assemblyinfo

---
+++ OpenTKExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenTK Extensions")]
+[assembly: AssemblyDescription("Adds some simple extensions on top of OpenTK.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Geoff Thornburrow")]
+[assembly: AssemblyProduct("OpenTKExtensions.Properties")]
+[assembly: AssemblyCopyright("Copyright ©2019 Geoff Thornburrow")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14B38FCE-C8D7-4972-99B7-A15DCDAA5E8C")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
